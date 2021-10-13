989986bcd7942714b6967f0ed3c100f93b22f7ab
hoangmanhcuong97 <cuong.hoangmanh23101997@gmail.com>
2021-10-13 10:00:11 +0700

    xong phuong thuc sinup

--- src/main/java/com/example/mangxahoi/controller/SignUpController.java
+++ src/main/java/com/example/mangxahoi/controller/SignUpController.java
@@ -1,5 +1,9 @@
 package com.example.mangxahoi.controller;
 
+import com.example.mangxahoi.model.Account;
+import com.example.mangxahoi.model.AppRole;
+import com.example.mangxahoi.model.Image;
+import com.example.mangxahoi.model.VerifiAccount;
 import com.example.mangxahoi.service.account.IServiceAccount;
 import com.example.mangxahoi.service.approle.IServiceAppRole;
 import com.example.mangxahoi.service.image.IServiceImage;
@@ -7,7 +11,15 @@ import com.example.mangxahoi.service.jwt.JwtService;
 import com.example.mangxahoi.service.post.IServicePost;
 import com.example.mangxahoi.service.verify.IVerifiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
+
+
+import javax.validation.Valid;
+import java.util.Base64;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -25,5 +37,32 @@ public class SignUpController {
     JwtService jwtService;
     @Autowired
     IVerifiService verifiService;
+    @Autowired
+    JavaMailSender javaMailSender;
 
+    @PostMapping("/signup")
+    public ResponseEntity<String> createAcc(@Valid @RequestBody Account account) {
+        String message = "";
+        AppRole role = serviceAppRole.findById(2L).get();
+        Image image = serviceImage.findById(1L).get();
+        account.setAvatar(image);
+        account.setRole(role);
+        account.setEnable(false);
+        if (serviceAccount.add(account)) {
+            String str = "GP Coder";
+            Account acc = serviceAccount.loadUserByEmail(account.getEmail());
+            String token = Base64.getEncoder().encodeToString(account.getEmail().getBytes());
+            VerifiAccount verifiAccount = new VerifiAccount();
+            verifiAccount.setIdAcc(acc.getId());
+            verifiAccount.setToken(token);
+            VerifiAccount newVerifi = verifiService.add(verifiAccount);
+            SimpleMailMessage sendmail = new SimpleMailMessage();
+            sendmail.setTo(account.getEmail());
+            sendmail.setSubject("Bấm vào link bên dưới để xác thực email!");
+            sendmail.setText("https://vilo-vn.herokuapp.com/account/verification/" + newVerifi.getId() + "/" + acc.getId() + "?token=" + token);
+            javaMailSender.send(sendmail);
+            message = "Check email to verification!";
+        } else message = "account exited!";
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
 }
