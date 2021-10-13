3287ee1cfa883c8cbb3e7e17ec5440df61964dcc
unknown <mittervan@gmail.com>
2021-10-13 10:27:03 +0700

    xong phuong thuc xac thuc email

--- src/main/java/com/example/mangxahoi/controller/SignUpController.java
+++ src/main/java/com/example/mangxahoi/controller/SignUpController.java
@@ -25,6 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Base64;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -107,5 +108,42 @@ public class SignUpController {
             httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         }
         return new ResponseEntity<>(result, httpStatus);
+    }
+
+    @PutMapping("/addroleandimage")
+    public ResponseEntity<String> fixRoleAndDefaultAvatar() {
+        List<AppRole> appRoles = (List<AppRole>) serviceAppRole.findAll();
+        List<Image> images = (List<Image>) serviceImage.findAll();
+        if (appRoles.size() == 0) {
+            AppRole admin = new AppRole();
+            admin.setId(1L);
+            admin.setRole("ROLE_ADMIN");
+            AppRole user = new AppRole();
+            user.setRole("ROLE_USER");
+            user.setId(2L);
+            serviceAppRole.save(admin);
+            serviceAppRole.save(user);
+        }
+        if (images.size() == 0) {
+            Image image = new Image();
+            image.setId(1L);
+            image.setPath("https://firebasestorage.googleapis.com/v0/b/filebase-70567.appspot.com/o/images%2F84156601_1148106832202066_479016465572298752_o.jpg?alt=media&token=4ca2d074-2b3b-4524-a017-e951067fa3f5");
+            serviceImage.save(image);
+        }
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+
+
+    @GetMapping("/verification/{id}/{idAcc}")
+    public ResponseEntity<String> verification(@RequestParam("token") String token, @PathVariable("id") Long id, @PathVariable("idAcc") Long idAcc) {
+        VerifiAccount verifiAccount = verifiService.findById(id).get();
+        Account account = serviceAccount.findById(idAcc).get();
+        if (verifiAccount.getToken().equals(token)) {
+            account.setEnable(true);
+            serviceAccount.save(account);
+            return new ResponseEntity<>("Bấm vào link để đăng nhập https://fakeface.netlify.app/", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Xác thực hết hiệu lực!", HttpStatus.OK);
     }
 }
