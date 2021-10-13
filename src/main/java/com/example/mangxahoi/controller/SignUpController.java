292296ff17261e0188b25eb8d8b30112487e1813
PC <hungpppp12@gmail.com>
2021-10-13 10:16:02 +0700

    completed login

--- src/main/java/com/example/mangxahoi/controller/SignUpController.java
+++ src/main/java/com/example/mangxahoi/controller/SignUpController.java
@@ -1,5 +1,6 @@
 package com.example.mangxahoi.controller;
 
+import com.example.mangxahoi.dto.LoginAccount;
 import com.example.mangxahoi.model.Account;
 import com.example.mangxahoi.model.AppRole;
 import com.example.mangxahoi.model.Image;
@@ -15,11 +16,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -64,5 +70,42 @@ public class SignUpController {
             message = "Check email to verification!";
         } else message = "account exited!";
         return new ResponseEntity<>(message, HttpStatus.CREATED);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(HttpServletRequest request, @RequestBody Account account) {
+        String result = "";
+        HttpStatus httpStatus = null;
+        try {
+            if (serviceAccount.checkLogin(account)) {
+                result = jwtService.generateTokenLogin(account.getEmail());
+                Account account1 = serviceAccount.loadUserByEmail(account.getEmail());
+                LoginAccount loginAccount = new LoginAccount();
+                loginAccount.setId(account1.getId());
+                loginAccount.setFullName(account1.getFullName());
+                loginAccount.setAvatar(account1.getAvatar());
+                loginAccount.setToken(result);
+                return new ResponseEntity(loginAccount, HttpStatus.OK);
+            } else {
+                result = "Wrong email or password or not verification";
+                httpStatus = HttpStatus.BAD_REQUEST;
+            }
+        } catch (Exception ex) {
+            result = "Server Error";
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return new ResponseEntity<>(result, httpStatus);
     }
 }
