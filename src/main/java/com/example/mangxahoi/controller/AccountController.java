0198e23dbb94235cc4a84482cf6e8ad977012c71
hoangmanhcuong97 <cuong.hoangmanh23101997@gmail.com>
2021-10-13 10:35:15 +0700

    xong phuong thuc create post

--- src/main/java/com/example/mangxahoi/controller/AccountController.java
+++ src/main/java/com/example/mangxahoi/controller/AccountController.java
@@ -1,5 +1,8 @@
 package com.example.mangxahoi.controller;
 
+import com.example.mangxahoi.model.Account;
+import com.example.mangxahoi.model.Image;
+import com.example.mangxahoi.model.Post;
 import com.example.mangxahoi.service.account.IServiceAccount;
 import com.example.mangxahoi.service.accountlike.IServiceLike;
 import com.example.mangxahoi.service.comment.IServiceComment;
@@ -9,6 +12,8 @@ import com.example.mangxahoi.service.jwt.JwtService;
 import com.example.mangxahoi.service.post.IServicePost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -31,5 +36,23 @@ public class AccountController {
     @Autowired
     IServiceFriend serviceFriend;
 
+    @PostMapping("/createPost")
+    public ResponseEntity<?> createPost(@RequestBody Image image) {
+        Post post = image.getPost();
+        Long idAc = post.getAccount().getId();
+        Account account = serviceAccount.findById(idAc).get();
+        post.setAccount(account);
+
+        Post newPost = servicePost.add(post);
+        image.setPost(newPost);
+        serviceImage.save(image);
+        return new ResponseEntity<>(servicePost.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/findPost/{idPost}")
+    public ResponseEntity<Post> findPostById(@PathVariable("idPost") Long idPost) {
+        Post post = servicePost.findById(idPost).get();
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
 
 }
