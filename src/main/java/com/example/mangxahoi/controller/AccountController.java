a6e03f674c0630cfb3d36917e13df2e824c189c3
PC <hungpppp12@gmail.com>
2021-10-13 10:40:05 +0700

    compeleted timeline

--- src/main/java/com/example/mangxahoi/controller/AccountController.java
+++ src/main/java/com/example/mangxahoi/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.example.mangxahoi.controller;
 
 import com.example.mangxahoi.model.Account;
+import com.example.mangxahoi.model.AccountLike;
 import com.example.mangxahoi.model.Image;
 import com.example.mangxahoi.model.Post;
 import com.example.mangxahoi.service.account.IServiceAccount;
@@ -11,6 +12,10 @@ import com.example.mangxahoi.service.image.IServiceImage;
 import com.example.mangxahoi.service.jwt.JwtService;
 import com.example.mangxahoi.service.post.IServicePost;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -53,6 +58,32 @@ public class AccountController {
     public ResponseEntity<Post> findPostById(@PathVariable("idPost") Long idPost) {
         Post post = servicePost.findById(idPost).get();
         return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
+    @PostMapping("/timeline")
+    public ResponseEntity<Page<Post>> timeline(@RequestBody String page) {
+        String[] sortById = new String[2];
+        Pageable pageable = PageRequest.of(Integer.parseInt(page), 5, Sort.by("id").descending());
+        Page<Post> postPage = servicePost.findPostByPrivacyContaining("public", pageable);
+        return new ResponseEntity<>(postPage, HttpStatus.OK);
+    }
+
+    @GetMapping("/likeshow/{idAcc}/{idPost}")
+    public ResponseEntity<?> createlike(@PathVariable("idAcc") Long idAcc, @PathVariable("idPost") Long idPost) {
+        AccountLike accountLike = serviceLike.findByAccount_IdAndPost_Id(idAcc, idPost);
+        if (accountLike != null) {
+            Long idlike = accountLike.getId();
+            serviceLike.remove(idlike);
+        } else {
+            Account account = serviceAccount.findById(idAcc).get();
+            Post post = servicePost.findById(idPost).get();
+            AccountLike accountLike1 = new AccountLike();
+            accountLike1.setAccount(account);
+            accountLike1.setPost(post);
+            serviceLike.save(accountLike1);
+        }
+        serviceLike.remove(accountLike.getId());
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
